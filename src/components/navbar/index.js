8e0f9ff4a79837846f23c1b9e677e5cfe8a0d924
Ovidiu Stoica <ovidiu.stoica1094@gmail.com>
2020-09-08 08:04:18 +0300

    Fixes to the navbar responsivness

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import Burger from './burger';
 import {Link} from 'gatsby';
 import Text from '../text';
+import {padding, width} from 'styled-system';
 
 const Nav = styled.nav`
   width: 100%;
   height: 70px;
   padding: 0 20px;
-  display: flex;
-  justify-content: space-between;
   position: fixed;
   background-color: rgba(255, 255, 255, 0.99);
   z-index: 98;
-  max-width: 1200px;
-  align-self: center;
+  max-width: 100%;
 
   margin-left: auto;
   margin-right: auto;
 
-  /* @media (min-width: 768px) {
+  ${padding}
+`;
+
+const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-left: auto;
+  margin-right: auto;
+  ${props =>
+    props.fullWidth &&
+    css`
+      width: 100%;
+      max-width: none !important;
+    `};
+  ${props =>
+    (props.noGutter &&
+      css`
+        padding-left: 0;
+        padding-right: 0;
+      `) ||
+    css`
+      padding-left: 30px;
+      padding-right: 30px;
+    `};
+  @media (min-width: 768px) {
     max-width: 750px;
     width: 100%;
   }
@@ -28,23 +50,36 @@ const Nav = styled.nav`
     width: 100%;
   }
   @media (min-width: 1220px) {
-    max-width: '1200px';
+    max-width: ${props => props.width || '1170px'};
     width: 100%;
   }
   @media (max-width: 768px) {
-    padding-left: 30px;
-    padding-right: 30px;
-  } */
+    ${props =>
+      props.mobileGutter &&
+      css`
+        padding-left: 30px;
+        padding-right: 30px;
+      `};
+  }
+
+  ${width}
 `;
 
 const Navbar = ({logo}) => {
   return (
     <Nav>
-      <Link href="/" style={logo}>
-        <Text className="page-title" content="Log" color="mainText" mb="0px" />
-        <Text className="page-title" content="o" color="accent" mb="0px" />
-      </Link>
-      <Burger />
+      <NavContainer width="1200px" noGutter>
+        <Link href="/" style={logo}>
+          <Text
+            className="page-title"
+            content="Log"
+            color="mainText"
+            mb="0px"
+          />
+          <Text className="page-title" content="o" color="accent" mb="0px" />
+        </Link>
+        <Burger />
+      </NavContainer>
     </Nav>
   );
 };
